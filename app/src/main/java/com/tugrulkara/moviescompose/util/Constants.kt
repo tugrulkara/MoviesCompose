34ffdba2937850679ccdfd0dbcf0fc5e5158cfe8
tugrulkara <74429693+tugrulkara@users.noreply.github.com>
2023-06-15 20:58:16 +0300

    Add Constants object with API and URL constants.

---
+++ app/src/main/java/com/tugrulkara/moviescompose/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.tugrulkara.moviescompose.util
+
+object Constants {
+
+    const val API_KEY="4a58fab2"
+    const val BASE_URL="http://www.omdbapi.com/"
+    const val IMDB_ID="imdb_id"
+}
