523c645b03aaf0e27d9dbd94ddf90674358195c0
tugrulkara <74429693+tugrulkara@users.noreply.github.com>
2023-06-16 19:39:04 +0300

    Add search bar and movie rows in MovieScreen view.

--- app/src/main/java/com/tugrulkara/moviescompose/presentation/movies/views/MovieScreen.kt
+++ app/src/main/java/com/tugrulkara/moviescompose/presentation/movies/views/MovieScreen.kt
@@ -1,14 +1,46 @@
 package com.tugrulkara.moviescompose.presentation.movies.views
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import com.tugrulkara.moviescompose.domain.model.Movie
+import com.tugrulkara.moviescompose.presentation.movies.MoviesEvent
 import com.tugrulkara.moviescompose.presentation.movies.MoviesViewModel
 
 @Composable
@@ -21,6 +53,123 @@ fun MovieScreen(
     Box(modifier = Modifier
         .fillMaxSize()
         .background(Color.Black)){
+
+        Column {
+            
+            MovieSearchBar(modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+                hint = "batman",
+                onSearch = {
+                    viewModel.onEvent(MoviesEvent.Search(it))
+            })
+            
+            LazyColumn(modifier = Modifier.fillMaxSize()){
+                items(state.movieList){movie->
+                    MoviesRow(movie = movie, onItemClick = {
+
+                    } )
+                }
+            }
+
+        }
+        
+    }
+    
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MovieSearchBar(
+    modifier: Modifier,
+    hint: String="",
+    onSearch: (String) -> Unit={}) {
+
+    var text by remember{
+        mutableStateOf("")
+    }
+
+    var isHintDisplay by remember {
+        mutableStateOf(hint != "")
+    }
+
+    Box(modifier = modifier){
+
+        TextField(
+            value =text,
+            onValueChange ={
+            text=it
+        }, keyboardActions = KeyboardActions(onDone = {
+            onSearch(text)
+            }),
+            maxLines = 1,
+            singleLine = true,
+            textStyle = TextStyle(color = Color.Black),
+            shape = RoundedCornerShape(12.dp),
+            colors = TextFieldDefaults.textFieldColors(Color.White),
+            modifier= Modifier
+                .fillMaxWidth()
+                .shadow(5.dp, CircleShape)
+                .background(color = Color.White)
+                .padding(horizontal = 20.dp)
+                .onFocusChanged {
+                    isHintDisplay = it.isFocused != true && text.isEmpty()
+                })
+
+        if (isHintDisplay){
+            Text(
+                text = hint,
+                color = Color.LightGray,
+                modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp)
+            )
+        }
+
+    }
+}
+
+@Composable
+fun MoviesRow(
+    movie:Movie,
+    onItemClick : (Movie)-> Unit) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                onItemClick(movie)
+            }
+            .padding(10.dp),
+        horizontalArrangement = Arrangement.SpaceBetween)
+    {
+
+        Image(
+            painter = rememberAsyncImagePainter(model = movie.Poster),
+            contentDescription =movie.Title,
+            modifier = Modifier
+                .padding(16.dp)
+                .size(200.dp, 200.dp)
+                .clip(RectangleShape)
+        )
+        
+        Column(
+            modifier = Modifier.align(CenterVertically),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = movie.Title,
+                style = MaterialTheme.typography.titleMedium,
+                overflow = TextOverflow.Ellipsis,
+                color = Color.White,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                text = movie.Year,
+                style = MaterialTheme.typography.titleSmall,
+                overflow = TextOverflow.Ellipsis,
+                color = Color.White,
+                textAlign = TextAlign.Center
+            )
+        }
 
     }
     
